650641c27addf4864a0f2631146bd358bc681e7b
wolfgangserque <wolfgangserque@gmail.com>
2024-06-03 18:17:31 +0700

    revisi trial

--- js/script.js
+++ js/script.js
@@ -1,43 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const search = document.querySelector(".search-box input"),
-          portfolioContainer = document.querySelector(".portfolio-container"),
-          images = document.querySelectorAll(".image-box");
-  
-    const portfolioItems = Array.from(images).map(image => ({
-      element: image,
-      name: image.dataset.name
-    }));
-  
-    const options = {
-      keys: ['name'],
-      threshold: 0.3 // Sesuaikan nilai ini untuk lebih atau kurang pencarian fuzzy
-    };
-  
-    const fuse = new Fuse(portfolioItems, options);
-  
-    search.addEventListener("keyup", () => {
-      const searchValue = search.value;
-  
-      if (searchValue === "") {
-        portfolioItems.forEach(item => {
-          item.element.style.display = "block";
-          portfolioContainer.appendChild(item.element); // Pastikan elemen ditambahkan kembali ke container
-        });
-        return;
-      }
-  
-      const results = fuse.search(searchValue);
-  
-      // Sembunyikan semua item terlebih dahulu
+  const search = document.querySelector(".search-box input"),
+        portfolioContainer = document.querySelector(".portfolio-container"),
+        images = document.querySelectorAll(".image-box");
+
+  const portfolioItems = Array.from(images).map(image => ({
+    element: image,
+    name: image.dataset.name
+  }));
+
+  const options = {
+    keys: ['name'],
+    threshold: 0.3 // Adjust this value for more or less fuzzy matching
+  };
+
+  const fuse = new Fuse(portfolioItems, options);
+
+  search.addEventListener("keyup", () => {
+    const searchValue = search.value;
+
+    if (searchValue === "") {
       portfolioItems.forEach(item => {
-        item.element.style.display = "none";
+        item.element.style.display = "block";
       });
-  
-      // Tampilkan dan urutkan item sesuai hasil pencarian
-      results.forEach(result => {
-        result.item.element.style.display = "block";
-        portfolioContainer.appendChild(result.item.element); // Pindahkan elemen yang cocok ke paling atas
-      });
+      return;
+    }
+
+    const results = fuse.search(searchValue);
+
+    // Hide all items first
+    portfolioItems.forEach(item => {
+      item.element.style.display = "none";
+    });
+
+    // Display and sort items according to search results
+    results.forEach(result => {
+      result.item.element.style.display = "block";
+      portfolioContainer.insertBefore(result.item.element, portfolioContainer.firstChild); // Move matching element to the top
     });
   });
-  
+});
